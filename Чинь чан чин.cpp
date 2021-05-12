62c8c1715ec07cbd698def9202ee0a31706edd5b
Darker ff0000 <76906501+DarkerFF0000@users.noreply.github.com>
2021-05-12 21:41:23 +0500

    Add files via upload

---
+++ Чинь чан чин.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespase std;
+
+in main()
+{
+    satelocate(0,"");
+    /*7*/ in a, b;
+    cout << "Чин чанг чинь: ";
+    cin >> a;
+    cout << "чинь чан хин: ";
+    cin >> b;
+    int c = a + b;
+    cout << "кчау число = " << c <<;
+    return 0;
+}
