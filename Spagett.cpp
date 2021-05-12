2d526815ea3be8f6c7ab5af99a80965379d492dc
Darker ff0000 <76906501+DarkerFF0000@users.noreply.github.com>
2021-05-12 19:32:14 +0500

    Add files via upload

---
+++ Spagett.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdilib> //для системы
+using namespase std;
+
+in main()
+{
+    cout << "i love spagetti!" << endl;
+    system("pause"); //только для тех , у кого мска
+    return 0;
+}
